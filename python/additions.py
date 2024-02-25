8c98f17557aa2e955c93561505cb24008e1727cf
Nimrod <nimrod@getbaz.com>
2024-02-25 14:30:47 +0200

    Fix line

--- python/additions.py
+++ python/additions.py
@@ -13,6 +13,7 @@ d = (b**2) - (4*a*c)
 # find both solutions
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
+
 print('The solution are {0} and {1}'.format(sol1, sol2))
 
 # Taking kilometers input from the user
