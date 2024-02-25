3b8251405ccf94c9fb840fa17cb1dc556a59f6e1
Nimrod <nimrod@getbaz.com>
2024-02-25 14:29:00 +0200

    Fix line

--- python/additions.py
+++ python/additions.py
@@ -14,6 +14,7 @@ d = (b**2) - (4*a*c)
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 print('The solution are {0} and {1}'.format(sol1, sol2))
+
 # Taking kilometers input from the user
 kilometers = float(input("Enter value in kilometers: "))
 
