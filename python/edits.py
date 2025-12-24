3f52e9dbfd5dd59dc1f43c1ca433760fc79487d5
Yuval Yacoby <33892515+yuvalyacoby@users.noreply.github.com>
2025-12-24 16:02:40 +0200

    Revert "test (#49)" (#50)
    This reverts commit 36f7f592525f0910f4c81df0d5d5f449ed6fbc9b.

--- python/edits.py
+++ python/edits.py
@@ -17,9 +17,8 @@ c += 3
 d = (b2 ** 2) - (4 * a * c)
 
 # find two solutions
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
-sol23 = (-b+cmath.sqrt(d))/(2*a)
+sol1 = (-b2 - cmath.sqrt(d)) / (2 * a)
+sol2 = (-b2 + cmath.sqrt(d)) / (2 * a)
 
 print('The solution are {0} and {1}'.format(sol1, sol2))
 
