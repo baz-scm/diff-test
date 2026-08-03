b8f6a05bc4e293e9635cc260c84e3ba868567909
Yuval Yacoby <yacoby133@gmail.com>
2026-08-03 12:04:07 +0300

    test: CR-4919 verify slack ticket context on prod

---
+++ node/cr-4919-prod-check.js
@@ -0,0 +1,8 @@
+function describeContextGaps(gaps) {
+  if (gaps.length === 0) {
+    return "all linked context was fetched"
+  }
+  return `missing context from: ${gaps.join(", ")}`
+}
+
+module.exports = { describeContextGaps }
